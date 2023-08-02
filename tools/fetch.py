5144d5e8d7f894b10048f620584e85a7d472d4d5
aiswarya98 <37261452+aiswarya98@users.noreply.github.com>
2023-08-02 16:21:09 +0530

    Update fetch.py

--- tools/fetch.py
+++ tools/fetch.py
@@ -7,6 +7,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ['KERAS_BACKEND'] = 'tensorflow'
 
 from cakechat.utils.env import init_keras
 
